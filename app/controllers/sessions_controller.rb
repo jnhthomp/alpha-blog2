fb0da47bfd60089f5b42fa5dc9db34e6f4099298
Jonah <jnh.thompson@gmail.com>
2020-09-07 02:11:27 -0700

    Adds Authentication helper methods
    They help set and maintain a user_id parameter in sessions
    This allows only certain actions to be available or certain menu items
    to show based on whether that session user_id is present or not
    
    This affectively sets an actual "logged in" status although we haven't
    restricted access to methods that a not logged in user shouldn't be able
    to access or restricted users to only be able to edit their own objects
    
    _navigation partial was updated to show login if session user_id doesn't
    exit
    If session user_id does exist it shows the relevant users username with
    links to go to their profile, edit their profile, and logout (clearing
    session user_id)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       flash[:notice] = "Login Was Successful"
       redirect_to user
     else
