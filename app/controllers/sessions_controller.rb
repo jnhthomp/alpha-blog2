34e20d79eefe58388127228b1ccf1c72ffed35a7
Jonah <jnh.thompson@gmail.com>
2020-09-06 23:03:47 -0700

    Implements sessions
    User can login with proper credentials and a session is created
    User can logout to destroy the current session
    
    Login and logout buttons added to navigation bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "Login Was Successful"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new'
+    end
   end
 
   def destroy
-    
+    session[:user_id] = nil
+    flash[:notice] = "User was logged out"
+    redirect_to root_path
   end
 end
