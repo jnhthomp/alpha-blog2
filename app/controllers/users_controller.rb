7c472d83e08b0d2648627897a72aa9ff7c9851e2
Jonah <jnh.thompson@gmail.com>
2020-09-06 17:53:22 -0700

    Cleans up layout
    Adds better linking to user profiles from articles index and
    individual article listing
    
    Includes user profile pic on individual article view
    
    Cleaned up some of users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.all
   end
@@ -18,18 +20,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to users_path(@user)
     else
       render 'edit'
     end
@@ -38,6 +39,11 @@ class UsersController < ApplicationController
 
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
