4823cd6c50b252c3c05e6499b41cfd14b697f1ea
Jonah <jnh.thompson@gmail.com>
2020-10-20 21:04:20 -0700

    Add pages controller tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    # Create a new user account to use for signing in
+    @user = User.create(username: "test_user", email: "test_user@email.com", password: "testadminpass", admin: false)
+  end
+
+  # Make sure root route is accessible to unregistered users
+  test "should get home if not logged in" do
+    # Go to the root route (pages#home)
+    get root_url
+    # Ensure that we get the home page
+    assert_response :success
+  end
+
+  # Make sure the root route is not accessible to registered users
+  test "should get articles index if logged in" do
+    # Sign in as the user from setup
+    sign_in_as(@user)
+    # Attempt to navigate to the home page
+    get root_url
+    # Confirm that the home page wasn't loaded and the user was redirected
+    assert_response :redirect
+  end
 end
